1509fa554ad73e39ff8992532c286954667e93ac
alokdev08-create <alokjavadev08@gmail.com>
2025-09-06 19:45:02 +0530

    Second commit

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("This is a TypeScript project.");
+// This is a simple TypeScript file that logs a message to the console.
+// You can add more functionality as needed.
+console.log("Hello, World!");
+var greeting = greet("TypeScript User");
+console.log;
+function greet(name) {
+    return "Hello, ".concat(name, "!");
+}
+//1 . Variables declaration and types
+var isDone = false;
+console.log(isDone);
+var decimal = 6;
+console.log(decimal);
+var color = "blue";
+console.log(color);
+var list = [1, 2, 3];
+console.log(list);
+var tuple = ["hello", 10];
+console.log(tuple);
---
+++ index.ts
@@ -0,0 +1,27 @@
+console.log("This is a TypeScript project.");
+// This is a simple TypeScript file that logs a message to the console.
+// You can add more functionality as needed.
+
+console.log("Hello, World!");
+let greeting: string = greet("TypeScript User");
+console.log;
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+//1 . Variables declaration and types
+let isDone: boolean = false;
+console.log(isDone);
+
+let decimal: number = 6;
+console.log(decimal);
+let color: string = "blue";
+console.log(color);
+let list: number[] = [1, 2, 3];
+console.log(list);
+let tuple: [string, number] = ["hello", 10];
+console.log(tuple);
+
+
+
+
